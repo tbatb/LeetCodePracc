3126d350bac1c1e097a232edb50224313f86bdbb
Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>
2023-11-25 18:14:09 +0100

    Time: 0 ms (100.00%), Space: 42.5 MB (7.11%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int runningsum = 0; 
+        int runningSum = 0; 
         
         for (int i = 0; i < nums.length; i++){
-            nums[i] = runningsum + nums[i];
+            nums[i] = runningSum + nums[i];
             
-            runningsum = nums[i];
+            runningSum = nums[i];
             //nums[i] += nums[i-1];
         }
         return nums;
