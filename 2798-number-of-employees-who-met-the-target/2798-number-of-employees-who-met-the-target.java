c5e4f68566e2af8d39b42c7842abe521a7dd477b
Tegshbayar Batbayar <tegshbayar.batbayar@outlook.com>
2023-11-25 01:09:37 +0100

    Time: 0 ms (100.00%), Space: 40.7 MB (86.68%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
         int counter = 0; 
-        String workedhours = "There are " + counter + " employees who met the target.";
+        //String workedhours = "There are " + counter + " employees who met the target.";
         for (int i = 0; i < hours.length; i++){
             if (hours[i] >= target){
                 counter++;
